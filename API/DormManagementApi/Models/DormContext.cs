b935efbb534518ff98677170fc9d49c3fb84b840
davidcristian <122480408+davidcristian@users.noreply.github.com>
2025-03-25 17:51:28 +0100

    Add api project skeleton

---
+++ API/DormManagementApi/Models/DormContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DormManagementApi.Models
+{
+    public class DormContext : DbContext
+    {
+        public DormContext() { }
+
+        public DormContext(DbContextOptions<DormContext> options) : base(options)
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Role> Role { get; set; }
+    }
+}
